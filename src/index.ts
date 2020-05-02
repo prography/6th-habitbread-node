dbc3c4f9c395bc95e39c02e14e112aef66b25565
wwlee94 <wwlee94@naver.com>
2020-05-03 04:47:06 +0900

    Add : schema.prisma order 변경 및 .env.test 설정 추가

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ if (ENV === 'prod') {
   dotenv.config({ path: `${__dirname}/../.env.prod` });
 } else if (ENV === 'dev') {
   dotenv.config({ path: `${__dirname}/../.env.dev` });
+} else if (ENV === 'test') {
+  dotenv.config({ path: `${__dirname}/../.env.test` });
 }
 
 const port: number = Number(process.env.PORT) || 3000;
