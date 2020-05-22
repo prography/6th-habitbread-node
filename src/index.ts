af5eca52cc1bb2ad0c680f4cec8390565c04aebc
wwlee94 <wwlee94@naver.com>
2020-05-22 14:52:32 +0900

    Add : letsencrypt로 ssl 인증서 발급 및 greenlock으로 자동 갱신

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import greenlock from 'greenlock-express';
 import app from './app';
 
 const ENV: string = process.env.NODE_ENV || 'dev';
@@ -10,7 +11,22 @@ if (ENV === 'prod') {
   dotenv.config({ path: `${__dirname}/../.env.test` });
 }
 
-const port: number = Number(process.env.PORT) || 3000;
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on ${port} at ${ENV} :)`);
-});
+greenlock
+  .init({
+    packageRoot: '{__dirname}/..',
+    configDir: './greenlock.d',
+
+    // contact for security and critical bug notices
+    maintainerEmail: 'wwlee94@naver.com',
+
+    // whether or not to run at cloudscale
+    cluster: false,
+  })
+  // Serves on 80 and 443
+  // Get's SSL certificates magically!
+  .serve(app);
+
+// const port: number = Number(process.env.PORT) || 3000;
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Server running on ${port} at ${ENV} :)`);
+// });
