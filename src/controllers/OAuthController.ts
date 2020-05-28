d3a390fc0e540ae1963e291054eef5575210f2bd
wwlee94 <wwlee94@naver.com>
2020-05-28 14:57:46 +0900

    Refactor : 리뷰 반영 및 사용하지 않는 apple oauth 라우터 삭제

--- src/controllers/OAuthController.ts
+++ src/controllers/OAuthController.ts
@@ -3,7 +3,7 @@ import AppleAuth, { AppleAuthAccessToken, AppleAuthConfig } from 'apple-auth';
 import { Response, urlencoded } from 'express';
 import fs from 'fs';
 import * as jwt from 'jsonwebtoken';
-import { Body, BodyParam, Get, HttpError, JsonController, Post, Res, UseBefore } from 'routing-controllers';
+import { Body, Get, HttpError, JsonController, Post, Res, UseBefore } from 'routing-controllers';
 import { BadRequestError, InternalServerError } from '../exceptions/Exception';
 import { AuthHelper } from '../middleware/AuthHelper';
 import { BaseController } from './BaseController';
@@ -28,14 +28,6 @@ export class OAuthController extends BaseController {
     return res.send(`<a href="${auth.loginURL()}">Sign in with Apple</a>`);
   }
 
-  // Client Secret 토큰 발급
-  @Get('/apple/token')
-  public async generateJwtToken() {
-    const result = await auth._tokenGenerator.generate();
-
-    return result;
-  }
-
   // Apple 서버로부터 callback 받는 라우터
   @Post('/apple/callback')
   public async appleOAuthCallback(@Body() body: Record<string, string>, @Res() res: Response) {
@@ -53,8 +45,7 @@ export class OAuthController extends BaseController {
         userName = `${name.lastName} ${name.firstName}`;
       }
 
-      let user;
-      user = await this.prisma.user.findOne({
+      let user = await this.prisma.user.findOne({
         where: {
           oauthKey: oauthKey,
         },
@@ -72,21 +63,6 @@ export class OAuthController extends BaseController {
 
       const token = AuthHelper.makeAccessToken(user.userId);
       return token;
-    } catch (err) {
-      if (err instanceof HttpError) return res.status(err.httpCode).send(err);
-      throw new InternalServerError(err.message || err);
-    }
-  }
-
-  // Refresh 토큰으로 Access 토큰 갱신
-  @Post('/apple/refresh')
-  public async updateRefreshToken(@BodyParam('refreshToken') refreshToken: string, @Res() res: Response) {
-    try {
-      if (refreshToken === null) throw new BadRequestError('Refresh 토큰이 없습니다.');
-
-      const accessToken = await auth.refreshToken(refreshToken);
-
-      return accessToken;
     } catch (err) {
       if (err instanceof HttpError) return res.status(err.httpCode).send(err);
       throw new InternalServerError(err.message || err);
