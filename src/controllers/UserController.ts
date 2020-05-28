adcd2cbf5d6e5b7459c8aa7120b29f70fa4c1485
wwlee94 <wwlee94@naver.com>
2020-05-28 12:29:31 +0900

    Refactor : User 컨트롤러 수정

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { validate } from 'class-validator';
-import { Response } from 'express';
-import { Body, Get, HttpError, JsonController, Post, Res } from 'routing-controllers';
+import { CurrentUser, Get, JsonController } from 'routing-controllers';
 import { v4 as uuid } from 'uuid';
-import { BadRequestError, InternalServerError } from '../exceptions/Exception';
-import { AddUser } from '../validations/UserValidation';
 import { BaseController } from './BaseController';
 const id: string = uuid();
 
@@ -25,28 +21,11 @@ export class UserController extends BaseController {
 
   // 사용자 전체 검색 API
   @Get('/users')
-  public async user() {
-    return await this.prisma.user.findMany();
-  }
-
-  // 사용자 추가 API
-  @Post('/users')
-  public async createUser(@Body() user: AddUser, @Res() res: Response) {
-    try {
-      const errors = await validate(user);
-      if (errors.length > 0) throw new BadRequestError(errors);
-
-      const createUser = await this.prisma.user.create({
-        data: {
-          name: user.name,
-          email: user.email,
-        },
-      });
-
-      return createUser;
-    } catch (err) {
-      if (err instanceof HttpError) return res.status(err.httpCode).send(err);
-      throw new InternalServerError(err.message);
-    }
+  public async user(@CurrentUser() currentUser: any) {
+    return await this.prisma.user.findOne({
+      where: {
+        userId: currentUser,
+      },
+    });
   }
 }
