2182499da4b6a7828eb479cbf4663ba387af6c25
wwlee94 <wwlee94@naver.com>
2020-06-01 03:25:42 +0900

    Refactor : Character -> UserItem Delete API 리팩터링

--- src/controllers/UserItemController.ts
+++ src/controllers/UserItemController.ts
@@ -1,8 +1,9 @@
 import { PrismaClient, User } from '@prisma/client';
 import { validate } from 'class-validator';
-import { CurrentUser, Get, HttpCode, HttpError, JsonController, Param, Params, Post } from 'routing-controllers';
+import { Body, CurrentUser, Delete, Get, HttpCode, HttpError, JsonController, Params, Post } from 'routing-controllers';
 import { BadRequestError, InternalServerError, NoContent, NotFoundError } from '../exceptions/Exception';
 import { ItemID } from '../validations/ItemValidation';
+import { UserItemID } from '../validations/UserItemValidation';
 import { BaseController } from './BaseController';
 
 @JsonController('/users/items')
@@ -21,6 +22,7 @@ export class UserItemController extends BaseController {
       const items = await this.prisma.userItem.findMany({
         where: { userId: currentUser.userId },
         select: {
+          userItemId: true,
           createdAt: true,
           item: true,
         },
@@ -35,26 +37,23 @@ export class UserItemController extends BaseController {
   }
 
   // 특정 사용자의 특정 아이템 조회 API
-  @Get('/:itemId')
-  public async findUserItem(@CurrentUser() currentUser: User, @Params() id: ItemID) {
+  @Get('/:userItemId')
+  public async findUserItem(@CurrentUser() currentUser: User, @Params() id: UserItemID) {
     try {
       const paramErrors = await validate(id);
       if (paramErrors.length > 0) throw new BadRequestError(paramErrors);
 
-      const item = await this.prisma.userItem.findMany({
-        where: {
-          userId: currentUser.userId,
-          itemId: id.itemId,
-        },
+      const item = await this.prisma.userItem.findOne({
+        where: { userItemId: id.userItemId },
         select: {
-          item: true,
+          userItemId: true,
           createdAt: true,
+          item: true,
         },
-        first: 1,
       });
-      if (item.length === 0) throw new NotFoundError('빵 아이템을 찾을 수 없습니다.');
+      if (item === null) throw new NotFoundError('빵 아이템을 찾을 수 없습니다.');
 
-      return item[0];
+      return item;
     } catch (err) {
       if (err instanceof HttpError) throw err;
       throw new InternalServerError(err.message);
@@ -62,18 +61,21 @@ export class UserItemController extends BaseController {
   }
 
   // 특정 사용자의 아이템 생성 API
-  // TODO: '/:itemId' 는 예시 추후 랜덤으로 사용자에게 빵 레벨별로 생성해야함!
-  @Post('/:itemId')
+  // TODO: 예시 추후 랜덤으로 사용자에게 빵 레벨별로 생성해야함!
+  @Post()
   @HttpCode(201)
-  public async createUserItem(@CurrentUser() currentUser: User, @Param('itemId') id: ItemID) {
+  public async createUserItem(@CurrentUser() currentUser: User, @Body() body: ItemID) {
     try {
+      const bodyErrors = await validate(body);
+      if (bodyErrors.length > 0) throw new BadRequestError(bodyErrors);
+
       return await this.prisma.userItem.create({
         data: {
           user: {
             connect: { userId: currentUser.userId },
           },
           item: {
-            connect: { itemId: id.itemId },
+            connect: { itemId: body.itemId },
           },
         },
       });
@@ -83,28 +85,19 @@ export class UserItemController extends BaseController {
     }
   }
 
-  // // 특정 캐릭터 삭제 API
-  // @Delete()
-  // public async deleteCharacter(@CurrentUser() currentUser: any) {
-  //   try {
-  //     const user = await this.prisma.user.findOne({
-  //       where: { userId: currentUser },
-  //       select: {
-  //         characters: true,
-  //       },
-  //     });
-  //     if (user === null) throw new NotFoundError('사용자를 찾을 수 없습니다.');
-  //     if (user.characters.length === 0) throw new NotFoundError('사용자의 캐릭터를 찾을 수 없습니다.');
+  // 특정 사용자의 아이템 삭제 API
+  @Delete('/:userItemId')
+  public async deleteCharacter(@CurrentUser() currentUser: User, @Params() id: UserItemID) {
+    try {
+      const paramErrors = await validate(id);
+      if (paramErrors.length > 0) throw new BadRequestError(paramErrors);
 
-  //     await this.prisma.character.delete({
-  //       where: {
-  //         characterId: user.characters[0].characterId,
-  //       },
-  //     });
-  //     return { message: 'Delete Character Success' };
-  //   } catch (err) {
-  //     if (err instanceof HttpError) throw err;
-  //     throw new InternalServerError(err.message);
-  //   }
-  // }
+      await this.prisma.userItem.delete({ where: { userItemId: id.userItemId } });
+
+      return { message: 'Delete Character Success' };
+    } catch (err) {
+      if (err instanceof HttpError) throw err;
+      throw new InternalServerError(err.message);
+    }
+  }
 }
