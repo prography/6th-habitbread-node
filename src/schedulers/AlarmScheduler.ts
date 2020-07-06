e6f57d9c052eba0a8288c648fbf3acffe5f1a562
dnatuna <dnatuna123@gmail.com>
2020-07-06 10:42:42 +0900

    Fix: 리뷰 내용 반영

--- src/schedulers/AlarmScheduler.ts
+++ src/schedulers/AlarmScheduler.ts
@@ -15,6 +15,17 @@ admin.initializeApp({
   databaseURL: 'https://habitbread-5abef.firebaseio.com',
 });
 
+const checkIndexOfNextDay = (todayIndex: number, dayOfWeek: string) => {
+  let indexOfNextDay = todayIndex + 1;
+  if (indexOfNextDay === 7) indexOfNextDay = 0;
+  while (dayOfWeek[indexOfNextDay] !== '1') {
+    indexOfNextDay++;
+    if (indexOfNextDay === 7) indexOfNextDay = 0;
+  }
+
+  return indexOfNextDay;
+};
+
 const scheduler = {
   //알람 메세지 전송
   SendAlarmJob: async () => {
@@ -26,42 +37,31 @@ const scheduler = {
         while (1) {
           const habitId = await redis.spop(moment().format('MMDDHHmm'));
           if (habitId === null) break;
-          const data = await redis.hmget(`habitId:${habitId}`, ['userId', 'title', 'dayOfWeek']);
-          const user = await redis.hmget(`userId:${data[0]}`, ['isAlarmOn', 'FCMToken']);
-          if (user[0] === '0') break;
+          const [userId, title, dayOfWeek] = await redis.hmget(`habitId:${habitId}`, ['userId', 'title', 'dayOfWeek']);
+          const [isAlarmOn, FCMToekn] = await redis.hmget(`userId:${userId}`, ['isAlarmOn', 'FCMToken']);
+          if (isAlarmOn === '0') break;
           const AMPM = moment().hours() >= 12 ? 'PM' : 'AM';
           const time = (type: string) => moment().format(type);
           const FCM: TokenMessage = {
             notification: {
               title: `습관빵 알람 - ${AMPM} ${time('hh')}:${time('mm')}`,
-              body: `${data[1]}빵을 구울 시간이에요!`,
+              body: `${title}빵을 구울 시간이에요!`,
             },
             data: {
               habitId: habitId,
             },
-            token: user[1],
+            token: FCMToekn,
           };
 
           await admin.messaging().send(FCM);
 
-          let check = moment().day() + 1;
-          if (check === 7) check = 0;
-          while (data[2][check] !== '1') {
-            check++;
-            if (check === 7) check = 0;
-          }
-
-          if (check <= moment().day()) {
-            check = 7 - moment().day() - check;
-            await redis.sadd(moment().add(check, 'days').format('MMDDHHmm'), habitId);
-            await redis.hmset(`habitId:${habitId}`, ['userId', data[0], 'title', data[1], 'dayOfWeek', data[2]]);
-            await redis.expire(`habitId:${habitId}`, 604860);
-          } else {
-            check = check - moment().day();
-            await redis.sadd(moment().add(check, 'days').format('MMDDHHmm'), habitId);
-            await redis.hmset(`habitId:${habitId}`, ['userId', data[0], 'title', data[1], 'dayOfWeek', data[2]]);
-            await redis.expire(`habitId:${habitId}`, 604860);
-          }
+          const indexOfNextDay = checkIndexOfNextDay(moment().day(), dayOfWeek);
+          let dateToAdd;
+          if (indexOfNextDay <= moment().day()) dateToAdd = 7 - moment().day() + indexOfNextDay;
+          else dateToAdd = indexOfNextDay - moment().day();
+          await redis.sadd(moment().add(dateToAdd, 'days').format('MMDDHHmm'), habitId);
+          await redis.hmset(`habitId:${habitId}`, ['userId', userId, 'title', title, 'dayOfWeek', dayOfWeek]);
+          await redis.expire(`habitId:${habitId}`, 604860);
         }
       } catch (err) {
         throw new InternalServerError(err.message);
