49164a0a71b6808bedc392eafb4d77858b8ffefb
wwlee94 <wwlee9410@gmail.com>
2020-07-07 12:37:40 +0000

    Add : PR 리뷰 반영

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,10 @@ ENV=${NODE_ENV} npm run prisma:generate
 echo "Apply Prisma migrations"  
 ENV=${NODE_ENV} npm run prisma:up
 
+# Running Alarm Scheduler
+echo "Running Alarm Scheduler"
+npx ts-node ./src/scripts/AddScheduleIntoRedis.ts
+
 # Run Server
 echo "Run Server !"
 npm start
